73b55f5f46acdbe28a21c0a243376c7acad6c79a
ravi11o <ravi.suraj110@gmail.com>
2018-09-17 19:38:25 +0530

    add package and description to mix

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Datex.MixProject do
       version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      package: package(),
+      description: description(),
       deps: deps()
     ]
   end
@@ -16,6 +18,28 @@ defmodule Datex.MixProject do
     [
       extra_applications: [:logger]
     ]
+  end
+
+  defp description do
+    """
+    Human readable simple date and time library.
+
+    It works with standard date and time as well as elixir date, time formats.
+
+    You can get a specific day, date, add days to date and get results which are friendly and easy to understand.
+
+    Compare dates and time, obtain results in formats you want to use. Convert any date, time formats to elixir formats and vice-versa.
+
+    Get current date, yesterday, tommmorow with or without time zones(utc by default).
+    """
+  end
+
+  defp package do
+    %{ licenses: ["MIT"],
+       maintainers: ["Suraj Kumar"],
+       links: %{ "GitHub" => "https://github.com/AltCampus/datex"},
+       files: ~w(lib mix.exs README* LICENSE*)
+     }
   end
 
   # Run "mix help deps" to learn about dependencies.
